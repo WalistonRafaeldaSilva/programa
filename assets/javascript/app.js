2e2171d0d1a83872bee9733468de1b9d498dc211
Waliston Rafael <programarafael34@gmail.com>
2023-10-30 20:57:20 -0300

    atualizado

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+import { portifolio } from "./../../data/data.js"
+const {username, role, resume, social} = portifolio
+
+let portifolioName = document.querySelector(".js-portifolio-name")
+let portifolioRole = document.querySelector (".js-portifolio-role")
+let resumeText = document.querySelector(".js-resume-text")
+let portifolioContacts = document.querySelector(".js-portifolio-contacts")
+let btnProjects = document.querySelector(".js-btn-projects")
+let btnclose = document.querySelector(".js-btn-close")
+
+portifolioName.innerText = username
+portifolioRole.innerText = role
+resumeText.innerText = resume
+
+console.log(portifolio.username)
+
+for(let i = 0; i <social.length; i++) {
+    let listItem = document.createElement("li")
+    let listItemImg = document.createElement("img")
+
+    listItemImg.src = social[i]
+    listItem.appendChild(listItemImg)
+    portifolioContacts.appendChild(listItem)
+}
+btnProjects.addEventListener("click", ()=> {
+    document.querySelector(".js-modal").style.display = "inline-block"
+})
+
+btnclose
---
+++ data/data.js
@@ -0,0 +1,21 @@
+const portifolio = {
+    username: "Waliston Rafael",
+    role: "Estudante",
+    social: ["./assets/imagens/github.svg", "./assets/imagens/email.svg", "./assets/imagens/linkedin.svg"],
+    resume: "Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+    "consectetur adipisicing elit.  laboriosam animi quisquam. Quam " +
+    "temdoloribus provident obcaecati voluptatem nesciunt ipsa et " +
+    "dignissimos corporis poribus dolor provident vitae, beatae magni!.",
+
+    projects: [
+        {name: "", tools: [], ur1: ""},
+        {name: "", tools: [], ur1: ""},
+        {name: "", tools: [], ur1: ""},
+        {name: "", tools: [], ur1: ""},
+        {name: "", tools: [], ur1: ""},
+        
+    ]
+
+}
+
+export {portifolio}
